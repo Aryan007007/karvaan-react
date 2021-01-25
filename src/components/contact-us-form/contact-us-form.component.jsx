81d7b24a470491d2fc61e5cdfffbd5c6c51892f5
aryan007007 <aryanasthana211@gmail.com>
2021-01-25 22:08:15 +0530

    Created the contact us page, embedded the google form for contact us

---
+++ src/components/contact-us-form/contact-us-form.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../contact-us-form/contact-us-form.styles.scss';
+
+const ContactUsForm = () => (
+    <div className='form'>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdckgJ2q9Qdef5fVhQ4WYYsS_cCemD_Qw7EH8kpauXB8iuDHA/viewform?embedded=true" frameBorder="0" width='100%' height='1000' marginHeight="0" marginWidth="0">Loading…</iframe>
+    </div>
+)
+
+export default ContactUsForm;
--- src/pages/contact-us/contactUs.component.jsx
+++ src/pages/contact-us/contactUs.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import NavBar from '../../components/nav-bar/nav-bar.component';
+import ContactUsForm from '../../components/contact-us-form/contact-us-form.component';
 import Footer from '../../components/footer/footer.component'
 
 const ContactUsPage = () => (
     <div>
         <NavBar />
+        <ContactUsForm />
         <Footer />
     </div>
 )
